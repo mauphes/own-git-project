76613cc65a79d501b2f99ce8312000e2483c19d5
mauphes <mauphes@gmail.com>
2016-10-06 01:35:52 +0300

    develop

--- src/routes/learn/Learn.js
+++ src/routes/learn/Learn.js
@@ -126,7 +126,7 @@ class Learn extends Component {
     console.log(counter());
     console.log(counter());
 
-    counter2(300);
+    counter2(600);
     console.log(counter2());
     console.log(counter2());
     console.log(counter2());
